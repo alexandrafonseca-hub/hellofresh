e1e063f021f12992e12cc0fa7e2ff71c381cbd09
alexandrafonseca-hub <68859026+alexandrafonseca-hub@users.noreply.github.com>
2020-08-08 20:47:58 +0200

    Update keyword_casewhen_logic.sql

--- keyword_casewhen_logic.sql
+++ keyword_casewhen_logic.sql
@@ -1,51 +1,33 @@
 
 , CASE
 
-	  WHEN Lower(gc.wrap_up_note) LIKE '%virus%' THEN 'Coronavirus'
-		WHEN Lower(gc.wrap_up_note) LIKE '%coronavirus%' THEN 'Coronavirus'
-		WHEN Lower(gc.wrap_up_note) LIKE '% covid%' THEN 'Coronavirus' 
+	-- assumption is that corona only does not appear (if corona always coronavirus)
+	WHEN Lower(gc.wrap_up_note) LIKE '%virus%' OR Lower(gc.wrap_up_note) LIKE '%covid%' THEN 'Coronavirus'
 		
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Refund' AND Lower(gc.wrap_up_note) LIKE '%not received%' THEN 'Refund not received'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Refund' AND Lower(gc.wrap_up_note) LIKE '%not received%' THEN 'Refund not received'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Refund' AND Lower(gc.wrap_up_note) LIKE '%not received%' THEN 'Refund not received'
-
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%not paid%' THEN 'Not paid'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%not paid%' THEN 'Not paid'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%not paid%' THEN 'Not paid'
-
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%paid%' THEN 'Paid'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%paid%' THEN 'Paid'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%paid%' THEN 'Paid'
-
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%payment plan%' THEN 'Payment plan'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%payment plan%' THEN 'Payment plan'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%payment plan%' THEN 'Payment plan'
-
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%manual%' THEN 'Manual payment'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%manual%' THEN 'Manual payment'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%manual%' THEN 'Manual payment' 
+	-- 'Refund not received'
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Refund' AND Lower(gc.wrap_up_note) LIKE '%not received%' THEN 'Refund not received'
 		
-		WHEN ed.country_group = 'BENELUXFR' AND gc.wrap_up_name = 'Payments - Other' AND Lower(gc.wrap_up_note) LIKE '%overview%' THEN 'Overview'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND gc.wrap_up_name = 'Payments - Other' AND Lower(gc.wrap_up_note) LIKE '%overview%' THEN 'Overview'
-		WHEN ed.country_group = 'UK' AND gc.wrap_up_name = 'Payments - Other' AND Lower(gc.wrap_up_note) LIKE '%overview%' THEN 'Overview'
-    
-		WHEN ed.country_group = 'BENELUXFR' AND Lower(gc.wrap_up_note) LIKE '%email%' THEN 'Email'
-		WHEN ed.country_group = 'BENELUXFR' AND Lower(gc.wrap_up_note) LIKE '%e-mail%' THEN 'Email'
-		WHEN ed.country_group = 'BENELUXFR' AND Lower(gc.wrap_up_note) LIKE '%e mail%' THEN 'Email'
-		WHEN ed.country_group = 'BENELUXFR' AND Lower(gc.wrap_up_note) LIKE '%mail%' THEN 'Email'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND Lower(gc.wrap_up_note) LIKE '%email%' THEN 'Email'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND Lower(gc.wrap_up_note) LIKE '%e-mail%' THEN 'Email'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND Lower(gc.wrap_up_note) LIKE '%e mail%' THEN 'Email'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND Lower(gc.wrap_up_note) LIKE '%mail%' THEN 'Email'
-		WHEN ed.country_group = 'UK' AND Lower(gc.wrap_up_note) LIKE '%email%' THEN 'Email'
-		WHEN ed.country_group = 'UK' AND Lower(gc.wrap_up_note) LIKE '%e-mail%' THEN 'Email'
-		WHEN ed.country_group = 'UK' AND Lower(gc.wrap_up_note) LIKE '%e mail%' THEN 'Email'
-		WHEN ed.country_group = 'UK' AND Lower(gc.wrap_up_note) LIKE '%mail%' THEN 'Email'
+	-- 'Not paid'
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%not paid%' THEN 'Not paid'
+	
+	-- 'Paid'
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Reactivation' AND Lower(gc.wrap_up_note) LIKE '%paid%' THEN 'Not paid'
 
-		WHEN ed.country_group = 'BENELUXFR' AND Lower(gc.wrap_up_note) LIKE '%gdpr%' THEN 'GDPR'
-		WHEN ed.country_group IN ( 'AU', 'NZ' ) AND Lower(gc.wrap_up_note) LIKE '%gdpr%' THEN 'GDPR'
-		WHEN ed.country_group = 'UK' AND Lower(gc.wrap_up_note) LIKE '%gdpr%' THEN 'GDPR'
-
+	-- Payment plan
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%payment plan%' THEN 'Payment plan'
 		
+	-- 'Manual payment'
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Outstanding' AND Lower(gc.wrap_up_note) LIKE '%manual%' THEN 'Manual payment'
+
+	-- 'Overview'
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND gc.wrap_up_name = 'Payments - Other' AND Lower(gc.wrap_up_note) LIKE '%overview%' THEN 'Overview'
+
+	-- Email
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND Lower(gc.wrap_up_note) LIKE '%mail%' THEN 'Email'
+	
+	-- GDPR
+	WHEN ed.country_group IN ('AU','NZ','BENELUXFR','UK') AND Lower(gc.wrap_up_note) LIKE '%gdpr%' THEN 'GDPR'
+
+
 	ELSE 'Other'
 	END AS 'Keyword'
